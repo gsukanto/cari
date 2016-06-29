95f18297c810e6d73b6129aa204a50e50914b30c
gregory <gregory.sukanto@gmail.com>
2016-06-29 09:40:02 +0700

    clean up the code

--- cari.pl
+++ cari.pl
@@ -4,102 +4,85 @@
 use Getopt::Std;
 use vars qw/%opt @file_find @dir_find/;
 
-&mulai;
-#---------------------------------
-sub mulai{
-        Getopt::Std::getopts('hrd:',\%opt);
-        my $str = $ARGV[0];
-        $str =~ s/^s+|s+$//ig;
+&main;
 
-        unless($str) {
-                &help;
-                return;
-        }
+sub main{
+    Getopt::Std::getopts('hrd:',\%opt);
+    my $str = $ARGV[0];
+    $str =~ s/^s+|s+$//ig;
 
-        system('clear');
+    return &help if !($str) || ($opt{h} || $str eq '');
 
-        if($opt{h} || $str eq ''){
-                &help;
-                return;
-        }
+    system('clear');
+    my $dir='.';
+    @file_find = ();
+    @dir_find = ();
 
-   my $dir='.';
-        if($opt{d}){
-                $dir = $opt{d};
-                $dir =~ s/\\/\//g;
-                $dir =~ s/\/$//;
-                unless(-d $dir){
-                        print "Can't open directory: $dir";
-                        return;
-                }
-        }
-        @file_find = ();
-        @dir_find = ();
+    if($opt{d}){
+        $dir = $opt{d};
+        $dir =~ s/\\/\//g;
+        $dir =~ s/\/$//;
+        return print "Can't open directory: $dir" if !(-d $dir);
+    }
+    
 
-        print "\nSearch String REGEX: \"$str\" found at:\n",("-" x 50),"\n";
-
-                $/=undef; &cari($dir,$str);
-
-        print "\n\n";
+    print "\nSearch String REGEX: \"$str\" found at:\n",("-" x 50),"\n";
+    $/=undef; 
+    &cari($dir,$str);
+    print "\n\n";
 }
-
-#---------------------------------
 
 sub cari{
-        my ($dir,$str) = (shift,shift);
+    my ($dir,$str) = (shift,shift);
 
-        unless(opendir (DIR,$dir)){
-                print "\n\ncan't open directory: $dir\n\n";
-                return;
+    return print "Can't open directory: $dir" if !(opendir (DIR,$dir));
+
+    my @file = sort(readdir(DIR));
+    close(DIR);
+    
+    my $f_org, $f;
+
+    foreach $f (@file){
+        $f = "$dir/$f";
+
+        next if($f=~ /\.$|\/\/\.\.$/);
+
+        if (-d $f){
+            $opt{'r'} && &cari($f,$str) || next;
         }
 
-        my @file = sort(readdir(DIR));
-        close(DIR);
-        my $f_org,$f;
-        foreach $f (@file){
-                $f = "$dir/$f";
+        next if !(-T $f);
 
-                next if($f=~ /\.$|\/\/\.\.$/);
-
-                if (-d $f){
-                        $opt{'r'} && &cari($f,$str) || next;
+        open(FILE, $f);
+            while(<FILE>){
+                if ($_ =~ /$str/is){
+                    $f =~ s/^\.\///;
+                    print $f,"\n";
                 }
-
-                next unless (-T $f);
-
-                open(FILE, $f);
-                        while(<FILE>){
-                                if ($_ =~ /$str/is){
-                                        $f =~ s/^\.\///;
-                                        print $f,"\n";
-                                }
-                        }
-           close(FILE)
-        }
+            }
+       close(FILE)
+    }
 }
 
 
-#---------------------------------
 
 sub help{
-
-                print qq~\
+    print qq~\
 Usage:
  cari [-h]
  cari [-d "/dir/subdir"] [-r] "regex_string"
 
- "regex_string"    : string yang ingin dicari dalam bentuk regular expression
+ "regex_string"    : wanted string, must be given in regex
  -h                : Help - This list
- -d ""/dir/subdir" : cari pada spesifik direktori,
-                     bila tidak ada maka direktory diasumsikan PWD atau "."
- -r                : cari file secara recursive
+ -d ""/dir/subdir" : fing to specific direktory,
+                     the default is PWD or "."
+ -r                : recursively find the file
 
- contoh:
+ example:
 
-  *cari pada "." file yang mengandung kata "nama"
+  *find in "." anyfile that have string "nama"
 
       \$ cari "nama"
 
 ~;
-
 }
